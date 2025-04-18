040142b7507a6a2dd63fa84987af114194b7ca27
rohitdhingra <er.rohit.dhingra@gmail.com>
2025-04-18 17:07:09 +0530

    changes

--- helloworld/src/main/java/com/learning/HelloworldApplication.java
+++ helloworld/src/main/java/com/learning/HelloworldApplication.java
@@ -1,5 +1,6 @@
 package com.learning;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloworldApplication {
 
+	@Value("${custom.name}")
+	private String name;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
 	@GetMapping
 	public String hello() {
-		return "Hello World";
+		return "Hello "+name+"!";
 	}
 
 }
